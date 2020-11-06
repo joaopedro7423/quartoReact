5ac2bc651245a24bf38059845bcf221f7d4d977c
Joao Pedro <802.018@alunos.unigran.br>
2020-11-06 09:30:38 -0300

    Estilos e add imagem

--- autoescola/src/components/Hero.js
+++ autoescola/src/components/Hero.js
@@ -1,14 +1,57 @@
 import React from 'react';
 
-const Hero = ({ title, children}) => (
-    <div>
+//import "./Hero.css";
+import styled,{css} from 'styled-components';
+
+const Root = styled.div`
+        ${props=>css`
+        background: url(${props.image}),rgba(0,0,0,0.4);
+        background-size:cover;
+        background-position: center;
+        `}
+`;
+
+
+const Title = styled.h1`
+font-family: "Poppins", sans-serif;
+font-weight: 700;
+letter-spacing: 2px;
+`;
+
+const Content = styled.div`
+
+p, 
+li{
+font-size: 20px;
+font-weight: 300;
+}
+
+ul{
+    list-style: none;
+    padding-left: 0;
+}
+
+li
+{&::before{
+    
+    content: "\\2713\\0020";
+}} 
+`;
+
+
+const Hero = ({image, title, children}) => (
+    <Root image={image}>
         <div>
-            <h1>{title}</h1>
+
+            <Title>{title}</Title>
+            <Content>
             {children}
+            </Content>
+         
             <button>Matricule-se agora</button>
         </div>
 
-    </div>
+    </Root>
 );
 
 export default Hero;
--- autoescola/src/stories/Hero.stories.js
+++ autoescola/src/stories/Hero.stories.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Hero from "../components/Hero";
+import fundoImage from "./assets/fundo.jpg";
+import fundo2Image from "./assets/user.jpg";
 
 export default {
   title: 'Hero',
@@ -8,13 +10,13 @@ export default {
 };
 
 export const usage = () => (
-  <Hero title="Ganhe sua liberdade para ir e vir">
+  <Hero title="Ganhe sua liberdade para ir e vir" image={fundoImage}>
     <p>A auto escola lider de aprovação</p>
   </Hero>
 );
 
 export const withList = () => (
-  <Hero title="Ganhe sua liberdade para ir e vir">
+  <Hero title="Ganhe sua liberdade para ir e vir"image={fundo2Image}>
     <ul>
       <li>dsadasdasdsaaaaaaaaaasdasdaskfhasuhfbhasuhfu</li>
       <li>dilasln sand nas ndnas ndasn ndasnjkd njkasd</li>
