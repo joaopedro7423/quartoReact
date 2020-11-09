3dc9d27bd57684cce23c16d5d611b42c150e66d3
Joao Pedro <802.018@alunos.unigran.br>
2020-11-09 08:37:30 -0300

    Helmet-react

--- autoescola/src/styles/GlobalStyle.js
+++ autoescola/src/styles/GlobalStyle.js
@@ -1,5 +1,6 @@
-
-import { createGlobalStyle } from 'styled-components';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { Helmet } from "react-helmet";
 import { BreakpointSize, breakAt } from "./Breakpoints";
 
 const GlobalStyle = createGlobalStyle`
@@ -67,5 +68,16 @@ const GlobalStyle = createGlobalStyle`
     }
     
 `;
+const GlobalStyleComposed = () => {
+  <>
+    <GlobalStyle />
+    <Helmet>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+    rel ="stylesheet"
+    />
+    </Helmet>
+  </>
+};
 
 export default GlobalStyle;
