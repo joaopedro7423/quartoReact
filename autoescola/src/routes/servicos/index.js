599ac8e4db8fcb2b03cebdf6a5f566e041cf87ed
Joao Pedro <802.018@alunos.unigran.br>
2020-11-17 15:02:24 -0300

    add página de errors

--- autoescola/src/routes/servicos/index.js
+++ autoescola/src/routes/servicos/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useParams} from 'react-router-dom';
  
 import ProductDetailPage from "../../components/pages/ProductDetail";
+import Error from "../../components/pages/Error";
+import Image from "../../draws/About";
 
 import {useScrollToTop} from "../../hooks/scroll";
 import {useProduct} from "../../hooks/products";
@@ -10,6 +12,14 @@ const ProductDetail = () => {
   useScrollToTop();
   const {slang} = useParams();
   const product =useProduct({slang});
+
+  if(!product){
+    return <Error 
+      title="Serviço não encontrado"
+      description ="Serviço não encontrado ou não disponivel"
+      image={<Image />}
+    />
+  }
 
     return <ProductDetailPage product = {product} />
 
